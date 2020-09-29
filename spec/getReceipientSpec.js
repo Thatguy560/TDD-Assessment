8c44da243991b7b4e7bf34be32d9f867199b20ff
Peter Stevens <Peterstevens510@gmail.com>
2020-09-29 16:52:34 +0100

    fixing small typo

--- spec/getReceipientSpec.js
+++ spec/getReceipientSpec.js
@@ -7,7 +7,7 @@ describe("Get Recipient", () => {
 
   it("Will return the second recipient if the position is '2'.", () => {
     expect(
-      getRecipient("@User_One @UserABC Have you seen this from @Userxyz?", 2)
+      getRecipient("@User_One @UserABC! Have you seen this from @Userxyz?", 2)
     ).toEqual("UserABC");
   });
 
@@ -19,7 +19,7 @@ describe("Get Recipient", () => {
 
   it("Will return an empty string if the recipient isn't found.", () => {
     expect(
-      getRecipient("@User_One @UserABC Have you seen this from @Userxyz?", 4)
+      getRecipient("@User_One @UserABC! Have you seen this from @Userxyz?", 4)
     ).toEqual("");
   });
 });
